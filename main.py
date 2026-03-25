4e84c75c42551831a9f3e216d4d522849ec53758
felipecameloss <felipecss@al.insper.edu.br>
2026-03-25 11:35:50 -0300

    commit final se deus quiser

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = "mongodb://mongo-connections:27017/transacao_db"
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 mongo = PyMongo(app)
 
 def validate_uuid(u):
